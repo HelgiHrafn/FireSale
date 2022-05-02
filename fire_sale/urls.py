109807bd7f3646d1d18babafdac2dc2bef5fa13f
HelgiHrafn <Helgia21@ru.is>
2022-05-02 15:20:28 +0000

    register page added

--- fire_sale/urls.py
+++ fire_sale/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('', include('firesale.urls')),
     path('firesale/', include('firesale.urls')),
     path('login/', include('login.urls')),
+    path('register/', include('register.urls'))
 
 ]
