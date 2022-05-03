e0ec0ecf1823b871f393021b453efa63f3a70aee
Gudmundur Kristjan <gummigudna@gmail.com>
2022-05-03 13:42:40 +0000

    frontpage butons

--- firesale/views.py
+++ firesale/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
 sell_items = [
-    {'name': 'Jordan skór', 'price': 15000},
-    {'name': 'Jordan skór', 'price': 12000},
-    {'name': 'Jordan skór', 'price': 15000},
-    {'name': 'Jordan skór', 'price': 12000},
-    {'name': 'Jordan skór', 'price': 15000},
-    {'name': 'Jordan skór', 'price': 15000},
-    {'name': 'Jordan skór', 'price': 12000},
-    {'name': 'Jordan skór', 'price': 15000},
-    {'name': 'Jordan skór', 'price': 12000},
-    {'name': 'Jordan skór', 'price': 15000},
+    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 500},
+    {'name': 'Jordan skór', 'price': 12000, 'highestbid': 0},
+    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 1850},
+    {'name': 'Jordan skór', 'price': 12000, 'highestbid': 2500},
+    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 250},
+    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 5000},
+    {'name': 'Jordan skór', 'price': 12000, 'highestbid': 0},
+    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 0},
+    {'name': 'Jordan skór', 'price': 12000, 'highestbid': 8000},
+    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 500},
 ]
 
 
