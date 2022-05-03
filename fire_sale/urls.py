1167f2230e992bac45a45a5394b738957e864d9a
Gudmundur Kristjan <gummigudna@gmail.com>
2022-05-03 15:10:44 +0000

    second navbar + my_profile

--- fire_sale/urls.py
+++ fire_sale/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('login/', include('login.urls')),
     path('register/', include('register.urls')),
     path('my_sales/', include('my_sales.urls')),
-    path('my_offers/', include('my_offers.urls'))
+    path('my_offers/', include('my_offers.urls')),
+    path('my_profile/', include('my_profile.urls'))
 ]
