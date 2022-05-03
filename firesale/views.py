6cf6533acb49f7851ebed865db4e2fc972b799f9
huxjoeflow <huxjoeflow@gmail.com>
2022-05-03 16:30:30 +0000

    firesale frontpage items css

--- firesale/views.py
+++ firesale/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
 from firesale.models import Item
-from firesale.models import Condition
-
-sell_items = [
-    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 500},
-    {'name': 'Jordan skór', 'price': 12000, 'highestbid': 0},
-    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 1850},
-    {'name': 'Jordan skór', 'price': 12000, 'highestbid': 2500},
-    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 250},
-    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 5000},
-    {'name': 'Jordan skór', 'price': 12000, 'highestbid': 0},
-    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 0},
-    {'name': 'Jordan skór', 'price': 12000, 'highestbid': 8000},
-    {'name': 'Jordan skór', 'price': 15000, 'highestbid': 500},
-]
 
 
 # Create your views here.
