4dca94de7b27ead971cc1e9887ebfe6441cf50e1
huxjoeflow <huxjoeflow@gmail.com>
2022-05-04 14:19:36 +0000

    create user

--- register/views.py
+++ register/views.py
@@ -4,7 +4,7 @@ from register.forms.user_form import UserCreateForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'register/index.html')
+    return render(request, 'register/create_user.html')
 
 
 def create_user(request):
@@ -12,7 +12,7 @@ def create_user(request):
         print(1)
     else:
         form = UserCreateForm()
-    return render(request, 'register/index.html', {
+    return render(request, 'register/create_user.html', {
         'form': form
     })
 
