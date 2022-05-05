8e8491e83c9295e4a6aba37d571c9db9f8d40c2b
HelgiHrafn <helgia21@ru.is>
2022-05-05 14:49:11 +0000

    braut profile

--- my_profile/views.py
+++ my_profile/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'my_profile/index.html')
+
+
+def edit_profile(request):
+    return render(request, 'my_profile/edit_profile.html')
