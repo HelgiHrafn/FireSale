45f249add154aea9b4e535ece1828073fb8ac075
huxjoeflow <huxjoeflow@gmail.com>
2022-05-05 17:10:05 +0000

    views

--- user/form/profile_form.py
+++ user/form/profile_form.py
@@ -6,6 +6,12 @@ class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         exclude = ['id', 'user', 'rating']
-
+        widgets = {
+            'user_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'first_name:': widgets.TextInput(attrs={'class': 'form-control'}),
+            'last_name:': widgets.TextInput(attrs={'class': 'form-control'}),
+            'email': widgets.TextInput(attrs={'class': 'form-control'}),
+            'password': widgets.PasswordInput(attrs={'class': 'form-control'}),
+ }
 
 
