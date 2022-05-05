43c81938fa4fe4809feb13c0d500e400b498521c
HelgiHrafn <helgia21@ru.is>
2022-05-05 15:18:50 +0000

    edit profile takkinn virkar

--- my_profile/urls.py
+++ my_profile/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/my_profile
     path('', views.index, name="my_profile-index"),
+    path('/edit_profile', views.edit_profile, name="my_profile-edit_profile")
 ]
