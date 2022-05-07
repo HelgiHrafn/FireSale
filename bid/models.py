ab5465d0bc8a1acfbe1188e0b430b3e96cf540ff
huxjoeflow <huxjoeflow@gmail.com>
2022-05-07 17:31:40 +0000

    minor

---
+++ bid/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+from firesale.models import Item
+
+
+# Create your models here.
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_amount = models.IntegerField()
+    bid_item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    bid_status = models.BooleanField(default=False)
