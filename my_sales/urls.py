ac419bc859d31e8a7cc754617a3b294e6a65236f
huxjoeflow <huxjoeflow@gmail.com>
2022-05-08 16:20:37 +0000

    Confirming sale, logic missing

--- my_sales/urls.py
+++ my_sales/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/my_sales
     path('', views.index, name="my_sales-index"),
+    path('<int:id>', views.get_item_by_id, name="item-details"),
+    path('<int:id>/accept_bid/<int:bid>', views.accept_bid, name="sell-confirm"),
 ]
--- my_sales/views.py
+++ my_sales/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from firesale.models import Item
+from bid.models import Bid
+
 
 # Create your views here.
 def index(request):
-    context = {'items': Item.objects.all().order_by('item_name')}
+    context = {'items': Item.objects.filter(item_seller_id=request.user.id).order_by('item_name')}
     return render(request, 'my_sales/index.html', context)
+
+
+def get_item_by_id(request, id):
+    highest_bid = get_item_highest_bid(id)
+    return render(request, 'my_sales/sell_details.html', {
+        'item': get_object_or_404(Item, pk=id),
+        'bid': highest_bid
+    })
+
+
+def get_item_highest_bid(id):
+    bid = Bid.objects.filter(bid_item_id=id)
+    if bid is None:
+        return bid
+    else:
+        bid = bid.order_by('bid_amount').last()
+        return bid
+
+
+def accept_bid(request, id, bid):
+    #Do something
+    return render(request, 'my_sales/sell_confirm.html')
+
