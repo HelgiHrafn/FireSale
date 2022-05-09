5ccd3d5186777a51400c2749bd5d0a654e680382
Gudmundur Kristjan <gummigudna@gmail.com>
2022-05-09 14:35:47 +0000

    Search bar function implemented

---
+++ static/js/search_items.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('#search_btn').on('click', function (e) {
+        e.preventDefault();
+        var searchText = $('#searchbar').val(); //Sækja value i search bar
+        $.ajax( {
+            url: '/firesale?search_filter=' + searchText,
+            type: 'GET',
+            success: function(resp) { // Populate-a gögnin hér
+                var newHtml = resp.data.map(d => {
+                    return `<div class="each item">
+                                <a href="/firesale/${d.id}">
+                                    <button class = button-single-item>
+                                    <span class="image-container">
+                                    <img class="item-sell-img" src="${d.firstImage}" alt="Image of iteam found via search query"/>
+                                    </span>
+                                    <span class="item-sell-name">${d.name}</span>
+                                    <span class="item-sell-price">${d.price}</span>
+                                    </button>
+                                </a>
+                            </div>`
+                });
+                $('.button-sell-all-items').html(newHtml.join(''));
+                $('#searchbar').val('');
+            },
+            error: function(xhr,status,error) {
+                // TODO: Nánari villuskýring
+                console.error(error)
+            }
+        })
+    });
+});
