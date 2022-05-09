4b1862de9d2be82d72f481147e10a67c13271d58
Gudmundur Kristjan <gummigudna@gmail.com>
2022-05-09 19:05:02 +0000

    search button temp fix

--- static/js/search_items.js
+++ static/js/search_items.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
                                 <a href="/firesale/${d.id}">
                                     <button class = button-single-item>
                                     <span class="image-container">
-                                    <img class="item-sell-img" src="${d.firstImage}" alt="Image of iteam found via search query"/>
+                                    <img class="item-sell-img" src="${d.firstImage}" alt="Image of item found via search query"/>
                                     </span>
                                     <span class="item-sell-name">${d.name}</span>
                                     <span class="item-sell-price">${d.price}</span>
