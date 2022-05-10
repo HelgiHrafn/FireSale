58a8cd9882452346fc0f5fa23938fecce7c1f078
huxjoeflow <huxjoeflow@gmail.com>
2022-05-10 18:04:34 +0000

    minor fix not done

--- fire_sale/context_processors.py
+++ fire_sale/context_processors.py
@@ -1,14 +1,16 @@
 # project/context_processors.py
-from user.models import Profile
+#from user.models import Profile
 # project/context_processors.py
 
-
 def user_img(request):
-    user = request.user.id
-    if user:
-        user_id = request.user.id
-        profile = Profile.objects.get(user_id=user_id)
-        profile_image = profile.profile_image
-        return {'profile_img': profile_image}
+    return {'profile_img': 'https://media.istockphoto.com/vectors/user-icon-flat-isolated-on-white-background-user-symbol-vector-vector-id1300845620?k=20&m=1300845620&s=612x612&w=0&h=f4XTZDAv7NPuZbG0habSpU0sNgECM0X7nbKzTUta3n8='}
+
+#def user_img(request):
+#    user = request.user.id
+#    if user:
+#        user_id = request.user.id
+#        profile = Profile.objects.get(user_id=user_id)
+#        profile_image = profile.profile_image
+#        return {'profile_img': profile_image}
 
 
