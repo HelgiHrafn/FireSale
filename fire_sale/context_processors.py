f540e0972a9ef4d449490936e27802bcbff86704
huxjoeflow <huxjoeflow@gmail.com>
2022-05-10 18:58:37 +0000

    Most ghetto solution on user image FML

--- fire_sale/context_processors.py
+++ fire_sale/context_processors.py
@@ -1,16 +1,14 @@
 # project/context_processors.py
-#from user.models import Profile
-# project/context_processors.py
+from user.models import Profile
+
 
 def user_img(request):
-    return {'profile_img': 'https://media.istockphoto.com/vectors/user-icon-flat-isolated-on-white-background-user-symbol-vector-vector-id1300845620?k=20&m=1300845620&s=612x612&w=0&h=f4XTZDAv7NPuZbG0habSpU0sNgECM0X7nbKzTUta3n8='}
-
-#def user_img(request):
-#    user = request.user.id
-#    if user:
-#        user_id = request.user.id
-#        profile = Profile.objects.get(user_id=user_id)
-#        profile_image = profile.profile_image
-#        return {'profile_img': profile_image}
-
-
+    user_id = request.user.id
+    if user_id:
+        profile_img = Profile.objects.get(user_id=user_id)
+        return {
+            'profile_image': profile_img,
+            }
+    else:
+        profile_img = Profile.objects.all()[:1].get()
+        return {'profile_image': profile_img}
