06d638856d668348fd5a42263927132553c974b1
Gudmundur Kristjan <gummigudna@gmail.com>
2022-05-12 13:23:55 +0000

    models updated, migrations made, character limits

--- bid/models.py
+++ bid/models.py
@@ -6,7 +6,7 @@ from firesale.models import Item
 # Create your models here.
 class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    bid_amount = models.IntegerField()
+    bid_amount = models.PositiveIntegerField(max_length=9)
     bid_item = models.ForeignKey(Item, on_delete=models.CASCADE)
     bid_status = models.BooleanField(default=False)
     bid_paid = models.BooleanField(default=False)
