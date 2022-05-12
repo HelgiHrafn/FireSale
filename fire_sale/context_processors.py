0bebcbe95a899cf31d238a454a73aef9e99297ef
huxjoeflow <huxjoeflow@gmail.com>
2022-05-12 08:09:38 +0000

    ghetto solution on profile img and rating gone

--- fire_sale/context_processors.py
+++ fire_sale/context_processors.py
@@ -2,13 +2,3 @@
 from user.models import Profile
 
 
-def user_img(request):
-    user_id = request.user.id
-    if user_id:
-        profile_img = Profile.objects.get(user_id=user_id)
-        return {
-            'profile_image': profile_img,
-            }
-    else:
-        profile_img = Profile.objects.all()[:1].get()
-        return {'profile_image': profile_img}
