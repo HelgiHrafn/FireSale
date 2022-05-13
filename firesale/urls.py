ba0623429e32e142d2a0da3b2dc1a34ec4eb7b5b
huxjoeflow <huxjoeflow@gmail.com>
2022-05-13 10:18:27 +0000

    pathing fix

--- firesale/urls.py
+++ firesale/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # http://localhost:8000/firesale
     path('', views.index, name="firesale-index"),
     path('<opt>', views.index, name="firesale-index"),
-    path('<int:id>', views.get_item_by_id, name="item-details"),
+    path('item/<int:id>', views.get_item_by_id, name="item-details"),
     path('item_bid/<int:id>/', views.bid_item_by_id, name="item-bid"),
 ]
